83c8baaf152cf2635c69cc60ec0c957b86c99785
ekmpa <emmakndp@gmail.com>
2026-01-13 21:15:54 -0500

    ± init commit

---
+++ exps/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=long  #unkillable #main #long
+#SBATCH --output=logs/test-%j.out
+#SBATCH --error=logs/test-%j.err
+#SBATCH --cpus-per-task=4                     # Ask for 4 CPUs
+#SBATCH --mem=64G
+#SBATCH --time=168:00:00    
+#SBATCH --job-name=test-exps
+
+module load python/3.10
+source ~/CrediNet/.venv/bin/activate
+
+uv run python experiments.py
+
